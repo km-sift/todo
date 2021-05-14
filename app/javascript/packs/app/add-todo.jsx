b3f519fe452122583be0e87d652a3c9a27a44a55
Andrew Nicholson <andrew.nicholson@chargeback.com>
2021-05-14 08:52:29 -0400

    you can input the new todo now

--- app/javascript/packs/app/add-todo.jsx
+++ app/javascript/packs/app/add-todo.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
+import Input from '@material-ui/core/Input';
 import Todo from './todo';
 
 const AddTodo = () => {
   const [todos, addTodo] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
   return (
     <>
       {todos.map((todo) => (
         <Todo {...todo}/>
       ))}
-      <AddIcon onClick={() => addTodo([...todos, { title: 'new todo' }])}/>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <Input value={newTodo} onChange={({ target: { value }}) => setNewTodo(value)} />
+        <AddIcon onClick={() => addTodo([...todos, { title: newTodo }])}/>
+      </div>
     </>
   );
 };
